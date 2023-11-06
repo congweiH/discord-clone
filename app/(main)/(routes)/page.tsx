8becea005b5a1a995472ad4482f55a4b840f3c0b
congwei1104 <congwei_huang@163.com>
2023-11-07 01:55:20 +0800

    添加背景切换

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,12 @@
+import { ModeToggle } from '@/components/mode-toggle';
 import { Button } from '@/components/ui/button';
 import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <div className=" text-red-500">
+    <div>
       <UserButton afterSignOutUrl="/" />
-      procted page
+      <ModeToggle />
     </div>
   );
 }
